5d4f21db3fc3d795893319a16621defa741a7f42
Aakash Pavar <aakashpavar87@gmail.com>
2024-05-28 13:42:29 +0530

    changed background of service section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,6 +1,7 @@
 import { Tally3 } from "lucide-react";
 import React from "react";
 import { ServiceCard } from "../utils/ServiceCard";
+import { BackgroundBeams } from "./ui/background-beams";
 
 function Services() {
   const cardItems = [
@@ -21,25 +22,46 @@ function Services() {
     },
   ];
   return (
-    <div className="mt-5 flex flex-col md:flex-col min-h-[100vh] bg-service bg-cover bg-center bg-no-repeat justify-center items-center font-google p-5 gap-3">
-      <span className="flex text-xl md:text-3xl text-white">
+    <div className="mt-5 flex flex-col md:flex-col min-h-[100vh]  bg-neutral-950 relative justify-center items-center font-google p-5 gap-3 antialiased">
+      <span className="flex z-10 text-xl md:text-3xl text-white">
         <Tally3 size={32} className="-skew-x-12 text-amber-500" />
         What We Offer
       </span>
-      <h3 className="text-xl md:text-3xl text-gray-300 font-google">
+      <h3 className="text-xl z-10 md:text-3xl text-gray-300 font-google">
         Start Your Journey with DevdaTours
       </h3>
-      <p className="text-sm w-full md:w-[40%] text-center text-gray-200">
+      <p className="z-10 text-sm w-full md:w-[40%] text-center text-gray-200">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
         autem perspiciatis, maiores ipsum nam, saepe nostrum facere placeat
         accusantium error tempora aut ducimus voluptas
       </p>
-      <div className="flex flex-col md:flex-row justify-center items-center gap-2 md:gap-5 px-6">
+      <div className="z-10 flex flex-col md:flex-row justify-center items-center gap-2 md:gap-5 px-6">
         {cardItems.map((item, index) => (
           <ServiceCard key={index} title={item.title} content={item.content} />
         ))}
       </div>
+      <BackgroundBeams />
     </div>
+    // <div className="mt-5 flex flex-col md:flex-col min-h-[100vh] justify-center items-center font-google p-5 gap-3 h-[40rem] w-full rounded-md bg-neutral-950 relative antialiased">
+    //   <span className="flex text-xl md:text-3xl text-white">
+    //     <Tally3 size={32} className="-skew-x-12 text-amber-500" />
+    //     What We Offer
+    //   </span>
+    //   <h3 className="text-xl md:text-3xl text-gray-300 font-google">
+    //     Start Your Journey with DevdaTours
+    //   </h3>
+    //   <p className="text-sm w-full md:w-[40%] text-center text-gray-200">
+    //     Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+    //     autem perspiciatis, maiores ipsum nam, saepe nostrum facere placeat
+    //     accusantium error tempora aut ducimus voluptas
+    //   </p>
+    //   <div className="flex flex-col md:flex-row justify-center items-center gap-2 md:gap-5 px-6">
+    //     {cardItems.map((item, index) => (
+    //       <ServiceCard key={index} title={item.title} content={item.content} />
+    //     ))}
+    //   </div>
+    //   <BackgroundBeams />
+    // </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 import { plugin } from "postcss"
 import serviceBackground from "./src/assets/"
+import { default as flattenColorPalette } from "tailwindcss/lib/util/flattenColorPalette";
 export default {
   content: [
     "./index.html",
@@ -15,8 +16,32 @@ export default {
       backgroundImage: {
         'service': "url('./src/assets/city2.jpg')",
         'hero': "url('https://images.unsplash.com/photo-1483728642387-6c3bdd6c93e5?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2676')",
-      }
+      },
+      animation: {
+        aurora: "aurora 60s linear infinite",
+      },
+      keyframes: {
+        aurora: {
+          from: {
+            backgroundPosition: "50% 50%, 50% 50%",
+          },
+          to: {
+            backgroundPosition: "350% 50%, 350% 50%",
+          },
+        },
+      },
     },
   },
-  plugin: []
+  plugin: [addVariablesForColors]
+};
+
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
 }
