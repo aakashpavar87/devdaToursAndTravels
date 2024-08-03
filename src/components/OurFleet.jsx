e33c970b42c76b2d398484bceda7655d72a879ad
Aakash Pavar <aakashpavar87@gmail.com>
2024-08-03 15:56:22 +0530

    Modified button text of order now to Book Now

--- src/components/OurFleet.jsx
+++ src/components/OurFleet.jsx
@@ -35,10 +35,12 @@ function OurFleet() {
           <motion.p className="font-bold text-xl md:text-6xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4">
             Ride in Style: Book Your Cab Now!
           </motion.p>
-          <button className="px-4 py-2 backdrop-blur-sm border bg-emerald-300/10 border-emerald-500/20 text-white mx-auto text-center rounded-full relative mt-4">
-            <span>Order now →</span>
-            <div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-emerald-500 to-transparent" />
-          </button>
+          <a href="tel:6355168597">
+            <button className="px-4 py-2 backdrop-blur-sm border bg-emerald-300/10 border-emerald-500/20 text-white mx-auto text-center rounded-full relative mt-4">
+              <span>Book Now →</span>
+              <div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-emerald-500 to-transparent" />
+            </button>
+          </a>
         </motion.div>
       </ImagesSlider>
     </div>
